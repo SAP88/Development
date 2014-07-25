2ce1ef5b95161846d037a47e7f050f4adb799d2b
SAP88 <xyliman@gmail.com>
2014-07-25 21:58:51 +0400

    RAbbitTest

---
+++ RabbitTest/Assets/Персонажы/Телка с мечом/AnimeCharactorController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimeCharactorController : MonoBehaviour 
+{
+    public float maxSpeed = 2f;
+    bool facingLeft = true;
+    bool grounded = false;
+    private Animator CharactorAnimator { get; set; }
+    public LayerMask whatIsGround;
+    public float jumpForce = 700f;
+    float groundRadius = 0.2f;
+    public Transform groundCheck;
+	// Use this for initialization
+	void Start () {
+        CharactorAnimator = this.GetComponent<Animator>();
+	}
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+        //CharactorAnimator.SetBool("Grounded", grounded);
+
+        var move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector3(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move < 0 && !facingLeft)
+        {
+            Flip();
+        }
+        else if (move > 0 && facingLeft)
+        {
+            Flip();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("JUMP!1");
+            //rigidbody2D.AddForce(new Vector2(0, jumpForce));
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y + 200);
+            //rigidbody2D.velocity.y = maxSpeed;
+            
+        }
+
+        CharactorAnimator.SetBool("Grounded", grounded);
+
+        if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Debug.Log("ATTACK!1");
+            CharactorAnimator.SetBool("AttackUpToBot", true);
+        }
+        else
+        {
+            CharactorAnimator.SetBool("AttackUpToBot", false);   
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            Debug.Log("ATTACK!2");
+            CharactorAnimator.SetBool("AttackFoward", true);
+        }
+        else
+        {
+            CharactorAnimator.SetBool("AttackFoward", false);
+
+        }
+	}
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
