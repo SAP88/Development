606a11a1955768a3598844a06a6bbf5eec5cd5f4
SAP88 <xyliman@gmail.com>
2014-07-27 23:06:03 +0400

    field + rocks

---
+++ RabbitTest/Assets/Замощение/RectangleObjectGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RectangleObjectGenerator : MonoBehaviour {
+    public GameObject Object;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void CreateObstacle()
+    {
+        Instantiate(Object);
+    }
+
+	// Update is called once per frame
+    //void Update () {
+	
+    //}
+}
